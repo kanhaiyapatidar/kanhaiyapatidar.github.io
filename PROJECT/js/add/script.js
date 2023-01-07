a3b1c233fde4d7bd004945f51ecf8e23e2650a6d
kanhaiyapatidar <kanhaiyapatidar94@gmail.com>
2023-01-07 17:37:21 +0530

    change

---
+++ PROJECT/js/add/script.js
@@ -0,0 +1,10 @@
+
+function blue() {
+    const a = document.getElementById("shyam")
+    a.style.backgroundColor = "blue"
+}
+
+function green() {
+    const b = document.getElementById("shyam")
+    b.style.backgroundColor = "green"
+}
